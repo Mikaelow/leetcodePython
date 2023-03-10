a17b47a512118b5f5a6107eec77aef47596288ab
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-10 20:40:36 +0100

    add new class to place there solution

---
+++ SoulutionNR9.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class SolNR9:
+    def mergeTwoLists(self, list1, list2):
+        counterList1 = Counter(list1)
+        counterList2 = Counter(list2)
+        newList = []
+        for i in range(10):
+            if counterList1[i] != 0:
+                for number in range(counterList1[i]):
+                    newList.append(i)
+            if counterList2[i] != 0:
+                for number in range(counterList2[i]):
+                    newList.append(i)
+        return newList

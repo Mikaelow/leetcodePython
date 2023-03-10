828546d74c0edb74f0aa780a7dde90f6dabc7483
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-10 20:40:51 +0100

    modified for solnr9

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from collections import Counter
+from SoulutionNR9 import SolNR9
 
-def mergeTwoLists(self, list1, list2):
-    counterList1 = Counter(list1)
-    counterList2 = Counter(list2)
-    newList = []
-    for i in range(10):
-        if counterList1[i] != 0:
-            for number in range(counterList1[i]):
-                newList.append(i)
-        if counterList2[i] != 0:
-            for number in range(counterList2[i]):
-                newList.append(i)
-    return newList
-list1 = []
-list2 = [0]
-
-print(mergeTwoLists('', list1, list2))
+list1 = [1,2,3,4]
+list2 = [1,2,3,8]
+print(SolNR9.mergeTwoLists('', list1, list2))

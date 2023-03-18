cd8ea5883313a4511858d6006cf1250fec09c436
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-18 01:01:14 +0100

    lengthOfLongestSubstring finished final form forma mutant

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -5,11 +5,13 @@ class SolNR3:
         curentStr = []
         for i in s:
             if i in curentStr:
+                counter = len(curentStr)
                 counterList.append(counter)
-                counter = 1
-            else:
-                counter = counter + 1
+                positionOfNextSameChar = curentStr.index(i)+1
+                for j in range(positionOfNextSameChar):
+                    curentStr.pop(0)
             curentStr.append(i)
+        counter = len(curentStr)
         counterList.append(counter)
         return max(counterList)
     def lengthOfLongestSubstring(self, s):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lengthOfLongestSubstring import SolNR3
 
 list1 = [1,4]
 list2 = [9,6]
-a='asjrgapa'
+a='abcabcbb'
 print(SolNR3().lengthOfLongestSubstring(a))
 
 

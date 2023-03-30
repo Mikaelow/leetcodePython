1e2a3ab727913512f466ba25545d197019bf9394
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-03-30 20:38:44 +0200

    zigzagConversion.py finished

--- zigzagConversion.py
+++ zigzagConversion.py
@@ -3,15 +3,19 @@ import math
 
 class SolNR6:
     def convert(self, s: str, howManyRows = 2):
+        table=self.fillTable(s,howManyRows)
+
+        return self.returningStrint(table)
+    def fillTable(self,s,howManyRows):
         numbColumns = self.howManyColums(s, howManyRows)
         motherTab = []
         j = 0
-        nextPosition=howManyRows-1
+        nextPosition = howManyRows - 1
         for _ in range(numbColumns):
             motherTab.append([])
-        for index,column in enumerate(motherTab):
+        for index, column in enumerate(motherTab):
             for i in range(howManyRows):
-                if nextPosition == howManyRows-1 or nextPosition==0:
+                if nextPosition == howManyRows - 1 or nextPosition == 0:
                     try:
                         column.append(s[j])
                         j += 1
@@ -19,15 +23,14 @@ class SolNR6:
                         pass
                 elif i == nextPosition:
                     column.append(s[j])
-                    j+=1
+                    j += 1
                 else:
                     column.append('')
             if nextPosition == 1 or nextPosition == 0:
-                nextPosition=howManyRows-1
+                nextPosition = howManyRows - 1
             else:
                 nextPosition -= 1
         return motherTab
-
     def howManyColums(self, s: str, numRows):
         counter = 0
         i = 0
@@ -38,3 +41,13 @@ class SolNR6:
                 i += 1
             counter += 1
         return counter - 1
+    def returningStrint(self,table: list):
+        string=''
+        for i in range(len(table[0])):
+            for j in range(len(table)):
+                try:
+                    string +=table[j][i] + ''
+                except:
+                    pass
+        return string
+

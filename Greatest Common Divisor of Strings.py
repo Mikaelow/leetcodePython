33f3466b13338d6be6de45d6ea2eb3d57b140eba
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-09-30 20:06:59 +0200

    To fix Greatest Common Divisor of Strings.py

--- Greatest Common Divisor of Strings.py
+++ Greatest Common Divisor of Strings.py
@@ -1,26 +1,38 @@
 class Solution:
-    def divisor(string : str) -> str:
-        memory = ''
-        for s in string:
-            if s in memory:
-                break
-            else:
-                memory = memory + s
-        return memory
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-        resoult = ''
-        shorter_string = min(str1,str2)
-        longer_string = max(str1,str2)
-        for char_low_str in shorter_string:
-            if char_low_str in longer_string:
-                resoult = resoult + char_low_str
-            else:
-                break
-        return Solution.divisor(resoult)
+    def resoult(self, word_to_divide:str):
+        divisor = 1
+        while len(word_to_divide)>=divisor:
+            start_of_string = 0
+            end_of_string = divisor
 
+            if len(word_to_divide)%divisor == 0:
+                memory = word_to_divide[start_of_string:end_of_string]
+
+                while end_of_string<=len(word_to_divide):
+                    if  memory == word_to_divide[start_of_string:end_of_string]:
+                       if end_of_string == len(word_to_divide):
+                           return memory
+                       start_of_string = start_of_string + divisor
+                       end_of_string = end_of_string + divisor
+                    else:
+                        break
+            else:
+                pass
+            divisor = divisor + 1
+
+
+    def gcdOfStrings(self, str1: str, str2: str):
+        result1 = self.resoult(str1)
+        result2 = self.resoult(str2)
+        if result1 == result2:
+            return result1
+        else:
+            return ''
+
+    
 
 
 if __name__ == '__main__':
-    str1 = "ABCDEF"
+    str1 = "ABCABC"
     str2 = "ABC"
     print(Solution().gcdOfStrings(str1,str2)) 

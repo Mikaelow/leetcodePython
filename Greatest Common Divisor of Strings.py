21475eb0a468cca5331ecedc00ce3e8dcc78dbd1
Mikaelow <114190199+Mikaelow@users.noreply.github.com>
2023-11-25 20:59:38 +0100

    update

--- Greatest Common Divisor of Strings.py
+++ Greatest Common Divisor of Strings.py
@@ -13,16 +13,34 @@ class Solution:
             while j<len(string):
                 if string[j] == base[i]:
                     resoult = resoult + string[j]
-                    j = 0
+                    j = 0                
                     break
                 else:
                     j = j + 1
             i = i + 1
-        
+        #abca abca  ||  abc
         return self.check_strings(resoult,base)
      
     def string_smaller_part(self, string : str):
-        return string
+        resoult = string[0]
+        positon_string = 0
+        positon_resoult = 0
+
+        while positon_string < len(string):
+            while positon_resoult < len(resoult):
+                char_string  = string[positon_string]
+                char_resoult = resoult[positon_resoult]
+
+                if char_resoult == char_string:
+                    positon_string += 1
+                    positon_resoult +=1
+                    resoult +=  string[positon_string] 
+                else:
+                    positon_string -= positon_resoult
+                    positon_resoult = 0   
+  
+            
+
         
 
     def gcdOfStrings(self, str1: str, str2: str):
